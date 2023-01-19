5fdea0857376ec657414ae72ddcf1c0dbf5ac760
ViniciusOliveira54 <viniciusoliveira.135.531@gmail.com>
2023-01-19 20:25:03 -0300

    Revert "adicionei comentario descritivo"
    This reverts commit 70f821018625f83c71a39ffc86c4078a4ba640b4.

--- conversor2.py
+++ conversor2.py
@@ -1,9 +1,9 @@
 # IHHX8rCReeK5nGqU1g9akAODLop9Xvfl
 import requests
 
-moeda = input (' Moeda que deseja converter : ').upper()
-convertor = input (' Para qual : ').upper()
-valor = input ('Valor : ')
+moeda = input (' moeda que deseja converter : ').upper()
+convertor = input (' para qual : ').upper()
+valor = input ('valor : ')
 
 url = f"https://api.apilayer.com/currency_data/convert?to={moeda}&from={convertor}&amount={valor}"
 
@@ -13,5 +13,5 @@ headers= {
 
 response = requests.request("GET", url, headers=headers )
 
-# converte o resultado para json e acessa a propriedade result
+
 print (response.json()["result"])
